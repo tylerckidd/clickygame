7379d7cf9510f8d2e92ace1445bb0d56cf1ea5ef
Tyler Kidd <tylerckiddcontact@gmail.com>
2019-06-05 18:20:09 -0400

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+//sets up the reusable Footer component
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+	<footer className = "footer">
+		<p>Tyler Kidd 2019</p>
+	</footer>
+);
+
+export default Footer;
